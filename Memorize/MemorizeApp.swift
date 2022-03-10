47e2bd3ed5083aa3787b10a99b6a661a3cc735e6
santos-enoque <santosenoque.ss@gmail.com>
2022-03-10 14:14:55 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Santos Enoque on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
